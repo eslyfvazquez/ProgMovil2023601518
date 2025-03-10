cade87b2398845a543d0a1acb1a9ec5aad0a923f
eslyfvazquez <eslyfabiola10@gmail.com>
2025-03-10 17:20:54 -0600

    MainActivity.kt

--- Practica1/MainActivity.kt
+++ Practica1/MainActivity.kt
@@ -1,19 +1,3 @@
-package com.example.practica1
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import java.time.LocalDate
-import java.time.Period
-import java.time.temporal.ChronoUnit
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
