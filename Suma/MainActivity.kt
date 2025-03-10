966b4ee0d6ed0995225b17df77b88627e2f75aac
eslyfvazquez <eslyfabiola10@gmail.com>
2025-03-10 17:23:12 -0600

    MainActivity.kt

--- Suma/MainActivity.kt
+++ Suma/MainActivity.kt
@@ -1,21 +1,3 @@
-package com.example.suma
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material3.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
